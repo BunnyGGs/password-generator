491b0d5ad1a1a1ab0ec0b8c983c435e03cfd88aa
BunnyGG <117492701+BunnyGGs@users.noreply.github.com>
2023-04-23 18:51:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const generatePassword = (length = 8) => {
+  let password = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+
+  return password;
+};
+
+console.log(generatePassword());
